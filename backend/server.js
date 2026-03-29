39313afe30516439b251af2af77c7502ab80b7a7
Sharanu <sharanu.co.in@gmail.com>
2026-03-30 00:16:22 +0530

    cors issue for previewed deploymeny

--- backend/server.js
+++ backend/server.js
@@ -114,21 +114,63 @@ const port = process.env.PORT || 4000;
 
 
 
-// CORS CONFIG
+
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://workconsole.vercel.app",
+//   "https://workconsole-r06z79ut2-sharanucoin-gmailcoms-projects.vercel.app"
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+
+//     // allow any vercel preview URL
+//     if (
+//       allowedOrigins.includes(origin) ||
+//       origin.includes("vercel.app")
+//     ) {
+//       return callback(null, true);
+//     }
+
+//     return callback(new Error("Not allowed by CORS"));
+//   },
+//   credentials: true
+// }));
+
+
+
+
+
+//  CORS CONFIG 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://workconsole.vercel.app"
+  "http://localhost:5173", // local
+  "https://workconsole.vercel.app" // production
 ];
 
-
 app.use(cors({
-  origin: (origin, callback) => {
+  origin: function (origin, callback) {
+    //  allow requests with no origin (Postman, mobile apps)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error("CORS blocked "));
+
+    //  allow exact origins
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    //  allow ALL vercel preview deployments
+    if (origin.includes("vercel.app")) {
+      return callback(null, true);
+    }
+
+    //  block everything else
+    return callback(new Error("Not allowed by CORS"));
   },
   credentials: true
 }));
+
+
 
 
 // Middleware
